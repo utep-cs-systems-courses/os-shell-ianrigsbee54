7a1457d35b3c2946794407af6451d621c8200e7a
ianrigsbee54 <irrigsbee@miners.utep.edu>
2021-02-14 19:39:03 -0700

    fixed exit check although can present future errors. simplified while loop condition, fixed os.execve line since it was all a string before. duh doi me

--- shell/shell.py
+++ shell/shell.py
@@ -1,36 +1,39 @@
 import os, sys, re
 from read import readLine
+
 prompt = '$ '
 
 def main():
     
-    running = True
-    while running:
+    while True:
         
         os.write(1,prompt.encode())
         
         Input = readLine()
-        Input = Input.split(' ')
+        Input = Input.split()
         
-        if Input == "exit":
-            os.write(2, ("Exiting shell\n").encode())
+        if len(Input) == 0:
+            sys.exit(0)
+            
+        if "exit" in Input:
+            os.write(1, ("Exiting shell\n").encode())
             sys.exit(0)
             
         rc = os.fork()
         
         if rc < 0:
-            os.write(2, "fork failed\n")
+            os.write(2, ("fork failed\n").encode())
             sys.exit(1)
             
         elif rc == 0:
             for dir in re.split(":", os.environ['PATH']):
-                program = "%s%s % (dir,Input[0])"
+                program = "%s/%s" % (dir, Input[0])
                 try:
                     os.execve(program, Input, os.environ)
                 except FileNotFoundError:
                     pass
                 
-            os.write(2, ("could not execute command %s\n % input[0])").encode())
+            os.write(2, ("could not execute command %s\n" % input[0]).encode())
             sys.exit(1)
             
         else:
