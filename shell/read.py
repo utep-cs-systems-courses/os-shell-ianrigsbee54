2a8767508cfa2abf5bdff581cae1568c03ffe209
ianrigsbee54 <irrigsbee@miners.utep.edu>
2021-03-02 21:49:43 -0700

    added comments in read.py and moved execute into its own method, along with adding comments

--- shell/read.py
+++ shell/read.py
@@ -1,30 +1,30 @@
 import os
 
-Buff = None
+Buff = None #we want to remember where we are in Buff if newline in middle
 
-def readChar():
+def readChars():
     global Buff
     
-    Buff = os.read(0, 500)
+    Buff = os.read(0, 500) #read up to 500 bytes
     
     return Buff
 
 def readLine():
     global Buff
-    string = ""
+    string = ""#start with empty string
     i = 0
     
-    if Buff == None:
-        Buff = readChar()
+    if Buff == None:#read chars by default
+        Buff = readChars()
     
     while len(Buff):
         string += chr(Buff[i])
         if "\n" in string:
-            Buff = Buff[i+1:]
+            Buff = Buff[i+1:]#cut string to next character 
             return string
         i += 1
         
-    if i == len(Buff):
+    if i == len(Buff):#if we reach end of Buff we reset
         i = 0
-        Buff = readChar()
+        Buff = readChars()
     return string
--- shell/shell.py
+++ shell/shell.py
@@ -4,17 +4,40 @@ from read import readLine
 
 prompt = '$ '
 Input = ""
+waitChild = True
 
 def execute(inputArg):
-    
-    for dir in re.split(":", os.environ['PATH']):
-        program = "%s/%s" % (dir, inputArg[0])
-        try:
-            os.execve(program, inputArg, os.environ)
-        except FileNotFoundError:
-            pass
+    rc = os.fork()
+    if rc < 0:
+        os.write(2, ("fork failed. Try again.").encode())
+        sys.exit(1)
+    if rc == 0:
+        if '/' in inputArg[0]: #for absolute paths
+            prog = inputArg[0]
+            try:
+                os.execve(prog, inputArg, os.environ)
+            except FileNotFoundError:
+                pass
+            os.write(2, ("%s : command not found\n" % (inputArg[0])).encode())
+            sys.exit(1)
+        if '<' in inputArg: #check for redirects
+            redirect("in")
+        elif '>' in inputArg:
+            redirect("out")
+        
+        for dir in re.split(':', os.environ['PATH']):
+            program = "%s/%s" % (dir, inputArg[0])
+            try:
+                os.execve(program, inputArg, os.environ)#have child execute program
+            except FileNotFoundError:
+                pass
+        
         os.write(2, ("%s : command not found\n" % (inputArg[0])).encode())
         sys.exit(1)
+    elif rc > 0:
+        if waitChild:#by default always wait for child
+            os.wait()
+        os.write(2, ("child finished\n").encode())
 
 def redirect(direction):
     #check redirect if input or output
@@ -22,21 +45,21 @@ def redirect(direction):
     
     if direction == "in":
         os.close(0)
-        os.open(Input[Input.index('<')+1], os.O_RDONLY)
+        os.open(Input[Input.index('<')+1], os.O_RDONLY)#open file, open for reading only
         os.set_inheritable(0, True)
-        Input.remove(Input[Input.index('<')+1])
+        Input.remove(Input[Input.index('<')+1])#remove associated characters
         Input.remove('<')
     if direction == "out":
         os.close(1)
-        os.open(Input[Input.index('>')+1], os.O_CREAT | os.O_WRONLY)
+        os.open(Input[Input.index('>')+1], os.O_CREAT | os.O_WRONLY)#create file if it doesnt exist
         os.set_inheritable(1, True)
         Input.remove(Input[Input.index('>')+1])
         Input.remove('>')
 
 def pipeInput(Inputarg):
     global Input
-    Input = Input.split('|')
-    leftArg = Input[0].split()
+    Input = Input.split('|')#split into two separate commands
+    leftArg = Input[0].split()#split just like regular string
     rightArg = Input[1].split()
     pipeRead, pipeWrite = os.pipe()
     rc = os.fork()
@@ -49,11 +72,11 @@ def pipeInput(Inputarg):
             redirect("in")         
         if '>' in leftArg:
             redirect("out")
-        os.close(1)
-        os.dup(pipeWrite)
-        os.set_inheritable(1, True)
+        os.close(1) #close 1 of fd which is connected to screen
+        os.dup(pipeWrite)#dup pipeWrite
+        os.set_inheritable(1, True)#allow child to use
         for fd in (pipeRead, pipeWrite):
-            os.close(fd)
+            os.close(fd)#close associated file descriptors
         execute(leftArg)
         
     else: #then exec the right arg here
@@ -61,15 +84,15 @@ def pipeInput(Inputarg):
             redirect("in")
         if '>' in rightArg:
             redirect("out")
-        os.close(0)
-        os.dup(pipeRead)
+        os.close(0)#close 0 of fd which is connected to keyboard
+        os.dup(pipeRead)#dup pipeRead
         os.set_inheritable(0, True)
-        for fd in (pipeRead, pipeWrite):
-            os.close(fd)
+        for fd in (pipeWrite, pipeRead):
+            os.close(fd)#close associated file descriptors
         execute(rightArg)
         
 def main():
-    global Input
+    global Input, waitChild
     
     while True:
         
@@ -87,39 +110,22 @@ def main():
         if "exit" in Input:
             os.write(1, ("Exiting shell\n").encode())
             sys.exit(0)
+        if '&' in Input:
+            waitChild = False
         if "cd" in Input:
             try:
                 if len(Input) == 1:
                     os.chdir("..")#go back a directory
+                    continue
                 else:
                     os.chdir(Input[1])#change to arguement directory
+                    continue
             except:#and if that directory doesnt exist throw an error
                 os.write(2, ("director %s: no such directory" % Input[1]).encode())
-        
-        rc = os.fork()
-        if rc < 0:
-            os.write(2, ("fork failed\n").encode())
-            sys.exit(1) 
-            
-        elif rc == 0:
-            
-            if '>' in Input: #check for redirect
-                redirect("out")
-            if '<' in Input:
-                redirect("in")
-            for dir in re.split(":", os.environ['PATH']):
-                program = "%s/%s" % (dir, Input[0])
-                try:
-                    os.execve(program, Input, os.environ)
-                except:
-                    pass
-            os.write(2, ("could not execute command %s\n" % Input[0].encode()))
-            sys.exit(1)
-                        
-        else:
-            childPidCode = os.wait()
-            os.write(1, ("Parent: Child %d terminated with exit code %d\n"%childPidCode).encode())
-                
+                sys.exit(1)
+        execute(Input)#exec program if it doesnt fit any of previous conditions
+        """
+        """        
 if __name__ == '__main__':
         main()
 
