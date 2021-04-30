ac54324d3284073df095693a478b9da0a4830b04
ianrigsbee54 <irrigsbee@miners.utep.edu>
2021-04-29 23:03:57 -0600

    final read fix

--- shell/read.py
+++ shell/read.py
@@ -2,26 +2,27 @@ from os import read
 
 index = 0
 limit = 0
+line = ""
 def getChar():
     global index, limit
-    if index == limit:
+    if index == limit: #initially 0=0
         index = 0
-        limit = read(0, 100)
-        if limit == 0:
+        limit = read(0, 100)#fill buffer
+        if limit == 0: #if nothing was inputted
             return "EOF"
-    if index < len(limit)-1:
-        c = chr(limit[index])
+    if index < len(limit)-1: #ensure we dont get index out of bounds error
+        c = chr(limit[index])#convert encoded byte to char and return it
         index+=1
         return c
     else:
-        return "EOF"
+        return "EOF"#once we reach the end, return EOF
 def readLine():
-    global index, limit
-    line = ""
+    global index, limit, line
+    line = "" #for our return string
     char = getChar()
-    while (char != '' and char != "EOF"):
-        line += char
+    while (char != '' and char != "EOF" and char != '\n'): #search for specific characters
+        line += char #put char into line and continue
         char = getChar()
-    index = 0
+    index = 0#once we are done with a line then set initial values to 0
     limit = 0
     return line
